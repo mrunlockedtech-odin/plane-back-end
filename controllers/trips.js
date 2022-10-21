bc68d2f521b8eef4d4c5d4857d7e77784fbee9ea
chriselliott97 <chris.elliott6497@gmail.com>
2022-10-21 08:54:00 -0400

    fix spelling error

--- controllers/trips.js
+++ controllers/trips.js
@@ -4,8 +4,8 @@ import { Trip } from "../models/trip.js"
 const create = async(req, res) => {
   try {
     req.body.owner = req.user.profile
-    const activity = await Trip.create(req.body)
-    res.status(201).json(activity)
+    const trip = await Trip.create(req.body)
+    res.status(201).json(trip)
   } catch (error) {
     console.log(error)
     res.status(500).json(error)
