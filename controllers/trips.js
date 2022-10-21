5bb0003adcfbf8cc6109bd93aa4b6bb4ecb4a7de
chriselliott97 <chris.elliott6497@gmail.com>
2022-10-20 20:12:00 -0400

    add functionality to trip controller

--- controllers/trips.js
+++ controllers/trips.js
@@ -2,7 +2,14 @@ import { Profile } from "../models/profile.js"
 import { Trip } from "../models/trip.js"
 
 const create = async(req, res) => {
-
+  try {
+    req.body.owner = req.user.profile
+    const activity = await Trip.create(req.body)
+    res.status(201).json(activity)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
 }
 
 
